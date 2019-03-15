22f86e6f740aecb21052ec5d0ee6dcd08cb7da4d
zgf <18819489648@163.com>
2019-03-15 18:29:35 +0800

    1.实现日志存储到mongodb

--- src/main/java/com/vanvov/kafka/listener/SimpleConsumer.java
+++ src/main/java/com/vanvov/kafka/listener/SimpleConsumer.java
@@ -7,6 +7,7 @@ import com.vanvov.kafka.service.ITSysLogService;
 import com.vanvov.kafka.service.IUserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,9 @@ public class SimpleConsumer {
     @Autowired
     ITSysLogService sysLogService;
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @KafkaListener(topics = {"basic-log"})
     public void receive(String message) throws InterruptedException {
         String substring = message.substring(message.indexOf("{"));
@@ -26,8 +30,9 @@ public class SimpleConsumer {
         if (StringUtils.isNotBlank(substring)){
             JSONObject jsonObject = JSONObject.parseObject(substring);
             TSysLog tSysLog = JSONObject.toJavaObject(jsonObject, TSysLog.class);
-            System.out.println(tSysLog);
             sysLogService.addTSysLog(tSysLog);
+            System.out.println("消费了"+JSONObject.toJSON(tSysLog).toString());
+            mongoTemplate.save(tSysLog, "log");
         }
 
     }
